c64adf28abe0cb5cd63d899bd83587c8767879fa
LeyCM <cm.ley.cm@gmail.com>
2025-10-06 17:10:47 +0200

    Remove loading and move it to core

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,12 +17,6 @@ pluginManagement {
 rootProject.name = "rub-config"
 
 include("api", "common")
-include("paper", "spigot")
-include("velocity", "bungee")
 
 project(":api").projectDir = file("rub-api")
 project(":common").projectDir = file("rub-common")
-project(":paper").projectDir = file("rum-paper")
-project(":spigot").projectDir = file("rum-spigot")
-project(":velocity").projectDir = file("rup-velocity")
-project(":bungee").projectDir = file("rup-bungee")
